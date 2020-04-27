ea95df62a57496b46e074a0ece882b27b3ed2544
sabina-music <64403851+sabina-music@users.noreply.github.com>
2020-04-27 12:08:40 +0200

    Update events.js

--- events.js
+++ events.js
@@ -1,1 +1,16 @@
+// JavaScript Document
 
+// "Cross-browser functions" enligt McPeak, J. & Wilton P. (2015) Beginning JavaScript, 5th ed., John Wiley & Sons, Inc., s.307ff
+
+// Lägg till en händelsehanterare
+// obj är elementet, type är händelsen och fn är funktionen
+function addListener(obj, type, fn) {
+	if (obj.addEventListener) obj.addEventListener(type,fn,false);
+	else obj.attachEvent("on"+type,fn);
+} // End addListener
+
+// Ta bort en händelsehanterare
+function removeListener(obj, type, fn) {
+	if (obj.removeEventListener) obj.removeEventListener(type,fn,false);
+	else obj.detachEvent("on"+type,fn);
+} // End removeListener
