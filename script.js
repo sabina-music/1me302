76c0643b63aacc025b579470aa9ebe72dd589b77
sabina-music <64403851+sabina-music@users.noreply.github.com>
2020-04-28 09:58:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+@charset "utf-8";
+/* CSS Document */
 
+html, body, div, header, nav, main, footer {margin:0; padding:0;}
+
+header, nav, main, footer {display: block;}
+
+/*------------------------------------------------------------------------logotyp header-------------------*/
+
+body{background-color:#C9C9C9; color:#000000;font-family:Baskerville, "Palatino Linotype", Palatino, "Century Schoolbook L", "Times New Roman", "serif";font-size:16px;}
+
+header{background-image: url("pics/?????.jpg");height:350px;max-width:100%;background-position:center;background-repeat:no-repeat;font-size:24px;font-style:italic;text-align:center; margin-top:0px;}
+
+     /*------------------------------------------------------------------------footer-------------------*/
+     footer {grid-area:footer; background-color:#AEAEAC; border-top:1px solid #F90; box-shadow:0px -5px 10px #999;
+	padding:4px; text-align:center;}
